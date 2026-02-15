ffff471493489c5de938aa757d4699e9a81caf43
INDRA-011 <indr.yznn.77@gmail.com>
2026-02-15 13:15:23 +0545

    routes added on the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,41 @@
-import { useEffect } from "react";
-import API from "./api/api";
+import { Routes, Route } from "react-router-dom";
+
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Courses from "./pages/Courses";
+import Marks from "./pages/Marks";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+
+import Layout from "./components/Layout";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  useEffect(() => {
-    API.get("/api/students")
-      .then((res) => console.log(res.data))
-      .catch((err) => console.error(err));
-  }, []);
-
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold text-red-500">React Running</h1>
-    </div>
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      {/* Protected Dashboard Routes */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Dashboard />} />
+        <Route path="courses" element={<Courses />} />
+        <Route path="marks" element={<Marks />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+    </Routes>
   );
 }
 
