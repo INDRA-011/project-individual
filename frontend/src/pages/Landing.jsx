9a655f790238844c8b90bfb35fbab3eea96b9d19
INDRA-011 <indr.yznn.77@gmail.com>
2026-02-16 09:57:43 +0545

    login and signin link on the landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,5 +1,15 @@
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <div>
+      <h1>Landing Page</h1>;
+      <a href="/login">
+        <h1>Login</h1>;
+      </a>
+      <a href="/signup">
+        <h1>Signup</h1>;
+      </a>
+    </div>
+  );
 };
 
 export default Landing;
