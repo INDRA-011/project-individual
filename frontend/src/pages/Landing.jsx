f5ea83698543da1883fd26b28c0e888c6b206668
INDRA-011 <indr.yznn.77@gmail.com>
2026-02-16 10:26:18 +0545

    syntax update

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,12 +1,12 @@
 const Landing = () => {
   return (
     <div>
-      <h1>Landing Page</h1>;
+      <h1>Landing Page</h1>
       <a href="/login">
         <h1>Login</h1>;
       </a>
       <a href="/signup">
-        <h1>Signup</h1>;
+        <h1>Signup</h1>
       </a>
     </div>
   );
